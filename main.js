7a847cc41237b52a221ffcad216e61e59072207b
ashtinh le <ashtinhle@hotmail.com>
2015-01-28 09:20:45 -0700

    first commit; basic nav bar and background so far

---
+++ main.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+    var links;
+
+    $.ajax({
+        url: 'nav.json',
+        dataType: 'json',
+        success: function(resp) {
+            links = resp.links;
+
+            var html = [];
+            for (var i = 0; i < links.length; i++) {
+                var link = links[i];
+                var $li =
+                    $("<li><a /><ul class='subLinks" + (i + 1) + "' id='subContainer" + (i + 1) + "'></ul></li>")
+                    .find("a").html(link.title)
+                    .attr({
+                        "title": link.title,
+                        "href": link.href,
+                        "id": link.id,
+                        "tabColor": link.tabColor
+                    })
+                    .end();
+                html.push($li);
+            }
+            $("#container").append(html);
+            getSubLinks();
+
+        },
+        error: function(req, status, err) {
+            console.log('something went wrong', status, err);
+        }
+    });
+
+    function getSubLinks() {
+        for (var i = 2; i < links.length; i++) {
+            var subHtml = [];
+
+            for (var j = 0; j < links[i].subsetLinks.length; j++) {
+
+                var subLink = links[i].subsetLinks[j];
+                var $subLi = $("<li><a /></li>").find("a")
+                    .html(subLink.title)
+                    .attr({
+                        "title": subLink.title,
+                        "href": subLink.href
+                            // "target": "_blank"
+                    })
+                    .end();
+                subHtml.push($subLi);
+            }
+            $("#subContainer" + (i + 1)).append(subHtml);
+        }
+
+        loadJquery();
+    };
+
+    function loadJquery() {
+        $("#subContainer3").hide();
+
+        $("#link3").on("click", function() {
+            $("#subContainer3").slideToggle("fast");
+            $(".pageTitle").toggle("fast");
+        });
+    }
+});
