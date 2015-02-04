1fec6509f5f1d88a371607a7d033c7088ce8177e
ashtinh le <ashtinhle@hotmail.com>
2015-02-04 10:34:23 -0700

    styled rss feeds. anarchy page is done.

--- rss.js
+++ rss.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     var FEED_URL = "http://www.internetnews.com/icom_includes/feeds/inews/xml_front-10.xml";
 
-        $.ajax({
-            url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(FEED_URL),
-            dataType: 'json',
-            success: function(data) {
-                if (data.responseData.feed && data.responseData.feed.entries) {
-                    $.each(data.responseData.feed.entries, function(i, e) {
-                        $("#rssFeeds").append("------------------------" + "<br/>");
-                        $("#rssFeeds").append("Title      : " + e.title + "<br/>");
-                        $("#rssFeeds").append("Link     : " + e.link + "<br/>");
-                    });
-                }
+    $.ajax({
+        url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(FEED_URL),
+        dataType: 'json',
+        success: function(data) {
+            if (data.responseData.feed && data.responseData.feed.entries) {
+                $.each(data.responseData.feed.entries, function(i, e) {
+                    $("#rssFeeds").append(e.title + "<br/>");
+                    $("#rssFeeds").append("<a target='_blank' href=\"" + e.link + "\">" + e.link + "</a><br/>");
+                    $("#rssFeeds").append("<br/>");
+                });
             }
-        });
+        }
+    });
 });
